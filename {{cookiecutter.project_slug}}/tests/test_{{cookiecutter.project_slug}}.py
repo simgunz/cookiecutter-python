53f98611d0dfcfe96bd5e6af7612f4be54f37e47
Simone Gaiarin <simgunz@gmail.com>
2022-02-05 14:57:47 +0100

    Add basic template

---
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -0,0 +1,5 @@
+from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    assert True
